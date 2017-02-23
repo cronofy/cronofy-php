085f8aa46850318db14c8e0b33cb531141271c4c
Harry Boyes <harry.boyes@gmail.com>
2017-02-23 11:42:45 +0000

    Increment version number

--- cronofy.php
+++ cronofy.php
@@ -19,7 +19,7 @@ class CronofyException extends Exception
 
 class Cronofy
 {
-    const USERAGENT = 'Cronofy PHP 0.8';
+    const USERAGENT = 'Cronofy PHP 0.9';
     const API_VERSION = 'v1';
 
     public $api_root_url;
