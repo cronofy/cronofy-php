75ba28b50fb21f24091a8f4dd8a0fad459ca7c57
Stephen Binns <stephen.binns@gmail.com>
2017-03-24 16:45:58 +0000

    Bump version

--- cronofy.php
+++ cronofy.php
@@ -19,7 +19,7 @@ class CronofyException extends Exception
 
 class Cronofy
 {
-    const USERAGENT = 'Cronofy PHP 0.11';
+    const USERAGENT = 'Cronofy PHP 0.12';
     const API_VERSION = 'v1';
 
     public $api_root_url;
