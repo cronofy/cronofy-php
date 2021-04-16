983817169b272534b24616ba9cd11b29ab9a4d42
Adam Whittingham <ad@cronofy.com>
2021-04-16 10:44:20 +0100

    Move smoke test to root

--- dev-smoke-test.php
+++ dev-smoke-test.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 use Cronofy\Batch\Batch;
 use Cronofy\Exception\CronofyException;
